4322e13d27d9e7ea4cbe4a5c69a1e696396b14e6
DizzlyD <david4henery00@hotmail.com>
2022-08-22 23:53:38 +0100

    basic-setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {Navbar} from "./components";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>App#1</h1>
     </div>
   );
---
+++ src/dummyData/tempCountryData.js
@@ -0,0 +1,36 @@
+import {tempFlag} from "../image/canada-flag.jpg"
+
+const tempCountryData = [
+    {
+        id: 1,
+        flag: tempFlag,
+        name: "canada",
+        population: "80,000,000",
+        region: "north america",
+        Capital: "Toronto",
+    },
+    {
+        id: 2,
+        flag: tempFlag,
+        name: "canada",
+        population: "80,000,000",
+        region: "north america",
+        Capital: "Toronto",
+    },
+    {
+        id: 3,
+        flag: tempFlag,
+        name: "canada",
+        population: "80,000,000",
+        region: "north america",
+        Capital: "Toronto",
+    },
+    {
+        id: 4,
+        flag: tempFlag,
+        name: "canada",
+        population: "80,000,000",
+        region: "north america",
+        Capital: "Toronto",
+    },
+]
